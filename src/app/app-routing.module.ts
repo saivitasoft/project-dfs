f77faf7b6b7ef6c87602094ed59a28e597719bd6
saivitasoft <saikumarvita@gmail.com>
2023-12-24 17:38:59 +0530

    crate login floder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexpageComponent } from './admin/indexpage/indexpage.component';
+import { LoginComponent } from './admin/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:IndexpageComponent},
+  {path:'login',component:LoginComponent}
+]
+
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexpageComponent } from './admin/indexpage/indexpage.component';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexpageComponent,
+    LoginComponent,
+    RegisterComponent,
+    
   ],
   imports: [
     BrowserModule,
